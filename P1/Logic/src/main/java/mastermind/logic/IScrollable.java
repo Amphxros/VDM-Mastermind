81e860626a768b3d32f761bef4cef423cda78a3e
AidaAcha <aidaacha@ucm.es>
2024-05-26 21:44:13 +0200

    comentario interfaz scroll

--- P1/Logic/src/main/java/mastermind/logic/IScrollable.java
+++ P1/Logic/src/main/java/mastermind/logic/IScrollable.java
@@ -4,6 +4,9 @@ package mastermind.logic;
  * Scroll interfaz
  */
 public interface IScrollable {
+    /**
+     * @param inputY: velocidad vertical del scroll
+     */
     void onVerticalScroll(float inputY);
 
 }
